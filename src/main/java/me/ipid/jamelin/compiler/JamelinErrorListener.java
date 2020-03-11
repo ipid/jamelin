e23bf28437350214b2ccfc54d0151429ee8ba7eb
ipid <ipid@users.noreply.github.com>
2020-03-12 01:11:34 +0800

    打印树、识别语法错误

---
+++ src/main/java/me/ipid/jamelin/compiler/JamelinErrorListener.java
@@ -0,0 +1,24 @@
+package me.ipid.jamelin.compiler;
+
+import org.antlr.v4.runtime.*;
+
+public class JamelinErrorListener extends BaseErrorListener {
+
+    boolean errorHappened = false;
+
+    @Override
+    public void syntaxError(
+            Recognizer<?, ?> recognizer,
+            Object offendingSymbol,
+            int line,
+            int charPositionInLine,
+            String msg,
+            RecognitionException e
+    ) {
+        errorHappened = true;
+    }
+
+    public boolean isErrorHappened() {
+        return errorHappened;
+    }
+}
---
+++ src/main/java/me/ipid/jamelin/entity/StateMachineNode.java
@@ -0,0 +1,5 @@
+package me.ipid.jamelin.entity;
+
+public class StateMachineNode {
+
+}
