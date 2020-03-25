75ee4f987fe63bcb308aaadc3ff1ab3945119374
ipid <ipid@users.noreply.github.com>
2020-03-25 22:50:03 +0800

    WIP：世界线变动失败

--- src/main/java/me/ipid/jamelin/entity/symbol/Proctype.java
+++ src/main/java/me/ipid/jamelin/entity/symbol/Proctype.java
@@ -7,40 +7,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Proctype implements PromelaNamedItem {
-    private StateMachine stateMachine;
-    private int id;
-    private List<MemorySlot> memoryLayout;
+    public final StateMachine stateMachine;
+    public final int id;
+    public final List<MemorySlot> memoryLayout;
 
     public Proctype(int id) {
         this.stateMachine = new StateMachine();
         this.id = id;
         this.memoryLayout = new ArrayList<>();
-    }
-
-    public StateMachine getStateMachine() {
-        return stateMachine;
-    }
-
-    public Proctype setStateMachine(StateMachine stateMachine) {
-        this.stateMachine = stateMachine;
-        return this;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public Proctype setId(int id) {
-        this.id = id;
-        return this;
-    }
-
-    public List<MemorySlot> getMemoryLayout() {
-        return memoryLayout;
-    }
-
-    public Proctype setMemoryLayout(List<MemorySlot> memoryLayout) {
-        this.memoryLayout = memoryLayout;
-        return this;
     }
 }
